19ffd605649bb02132720caba4500bec5ae90577
byxiaorun <byxiaorun@users.noreply.github.com>
2018-10-20 21:17:06 +0800

    home

---
+++ js/music.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+ 
+
+   function a(){
+
+     var audio = document.getElementById('music'); 
+
+     if(audio.paused){                 
+
+         audio.play();//audio.play();// 播放  
+
+     }
+
+     else{
+
+          audio.pause();// 暂停
+
+     } 
+
+   }
