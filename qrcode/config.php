0313358362d7ad97d4c63f34e908461c2c8f8322
byxiaorun <byxiaorun@users.noreply.github.com>
2018-10-20 21:40:39 +0800

    QR二维码
    QR

---
+++ qrcode/config.php
@@ -0,0 +1,9 @@
+<?php
+//支付宝链接
+$alipay_url = 'https://qr.alipay.com/tsx06614hcohpjl6qra3c22';
+//微信支付链接
+$wechat_url = 'https://payapp.weixin.qq.com/qr/AQE6DUEcUlWkOyHVkNs_eAzD?t=AQAC#wechat_pay';
+//qq支付链接
+$qq_url     = 'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&u=1208638621&a=1&n=%CC%B6%CC%B6%CC%B6%CC%B6%E7%AC%A8%CC%B6%E8%9B%8B%CC%B6%E4%B8%80%CC%B6%E4%B8%AA%CC%B6&ac=D3E46890F5310088E5C4A854CE330D9F4D54816FED899014ECEE64FAB45B1601';
+//京东金融支付链接
+$jd_url     = 'https://3.cn/IfvPIUy';
